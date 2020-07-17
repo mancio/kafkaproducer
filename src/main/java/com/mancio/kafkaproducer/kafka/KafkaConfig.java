5fd0f077b4ac9fd0350b3eb2185048854ee1c213
Andrea Mancini <mancioman2@gmail.com>
2020-07-17 17:01:51 +0200

    try to fix server seem always hardwired

--- src/main/java/com/mancio/kafkaproducer/kafka/KafkaConfig.java
+++ src/main/java/com/mancio/kafkaproducer/kafka/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.mancio.kafkaproducer.kafka;
 import com.mancio.kafkaproducer.pojo.EmployeesRating;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,10 +17,14 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+
     @Bean
     public ProducerFactory<String, EmployeesRating> producerFactory(){
         Map<String,Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory(config);
